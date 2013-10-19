1c2906f8c72f69388bf73c4768b016f51f71b0fa
Codemis <johnathan@missionaldigerati.com>
2013-10-18 20:28:34 -0700

    Added a way to create a dialog for each Annotation.

--- app/assets/javascripts/annotator.js
+++ app/assets/javascripts/annotator.js
@@ -1,4 +1,15 @@
 /* This file holds all the code for the annotation tools */
 $(document).ready(function() {
-	
+
 });
+/*
+ * Returns an JQuery instance of the dialog window
+ *
+ * @param Object content A JQuery object to append to the content
+ * @return Object
+ */
+function getDialogFrame(content) {
+	var widgetFrame = $("#dialog_template").clone(true, true);
+	widgetFrame.find('.content').append(content).removeClass('hidden');
+	return widgetFrame;
+};
