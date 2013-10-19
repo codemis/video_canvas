a4a8170d96652c98f291797904458b92cd43f06b
naysayer <joshuagregoryshuster@gmail.com>
2013-10-18 18:53:45 -0700

    progress

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+
+	def create_guest_user
+		puts "cheesecake"
+		puts params[:username]
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Videocanvas::Application.routes.draw do
   devise_for :users
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+
+  put '/create_guest_user' => 'users#create_guest_user', as: :create_guest_user
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
