6e6f4250ed50fc17c8d201dc053b8a4523b98a35
Cynthia Kiser <cnk@caltech.edu>
2013-10-19 13:13:46 -0700

    don't make controller specific asset files

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module Videocanvas
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # don't create controller specific stylesheets and javascript files
+    config.generators do |g|
+      g.assets false
+    end
   end
 end
