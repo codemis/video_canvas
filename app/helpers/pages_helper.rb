f51f591d0fec4fa7383eaf7065e95d67ef7a3b13
Cynthia Kiser <cnk@caltech.edu>
2013-10-20 16:06:18 -0700

    move text rotation into the footer so we can change images - in an asset pipeline munged fashion

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,15 @@
 module PagesHelper
+  def taglines
+    ["mustaches", "fart clouds", "stick figures", "unibrows", "angry eyes", "lols", "squiggly smell lines", "crazy hats", "eye patches", "lightening bolts", "hearts"] 
+  end
+  
+  def overlay_images
+    img_array = []
+    taglines.each do |n|
+      name = n.gsub(/[ \.]/, '')
+      img_array << '"'+name+'":"'+image_path("#{name}.png")+'" '
+    end 
+    '{'+ img_array.join(', ') + '}'
+  end
+
 end
