941c1129d083304c9af5135a25667a1ebc9a1ea7
Codemis <johnathan@missionaldigerati.com>
2013-10-20 13:30:05 -0700

    Fixed the time scrubber on the dialogs.

--- app/assets/javascripts/annotator.js
+++ app/assets/javascripts/annotator.js
@@ -453,22 +453,22 @@ function togglePinSlider(pin, annotationType) {
 		if (currentAnnotations[annotationUUID]['options'].hasOwnProperty('stopTime')) {
 			currentAnnotations[annotationUUID]['options']['stopTime'] = videoDuration;
 		}
-		$('span.start_time').text(toHHMMSS(0));
-		$('span.stop_time').text(toHHMMSS(videoDuration));
+		sliderDiv.find('span.start_time').text(toHHMMSS(0));
+		sliderDiv.find('span.stop_time').text(toHHMMSS(videoDuration));
 		saveAnnotation(annotationElement, annotationType);
 	} else {
 		/*
 		 * Set the defaultstart and stop times
 		 */
 		if (currentAnnotations[annotationUUID]['options'].hasOwnProperty('startTime')) {
-			$('span.start_time').text(toHHMMSS(currentAnnotations[annotationUUID]['options']['startTime']));
+			sliderDiv.find('span.start_time').text(toHHMMSS(currentAnnotations[annotationUUID]['options']['startTime']));
 		} else {
-			$('span.start_time').text(toHHMMSS(0));
+			sliderDiv.find('span.start_time').text(toHHMMSS(0));
 		}
 		if (currentAnnotations[annotationUUID]['options'].hasOwnProperty('stopTime')) {
-			$('span.stop_time').text(toHHMMSS(currentAnnotations[annotationUUID]['options']['stopTime']));
+			sliderDiv.find('span.stop_time').text(toHHMMSS(currentAnnotations[annotationUUID]['options']['stopTime']));
 		} else {
-			$('span.stop_time').text(toHHMMSS(videoDuration));
+			sliderDiv.find('span.stop_time').text(toHHMMSS(videoDuration));
 		}
 		$(pin).attr('data-showing', 'YES');
 		$(pin).addClass('pinned').removeClass('unpinned');
@@ -491,8 +491,8 @@ function togglePinSlider(pin, annotationType) {
 				pausePlayer(ui.values[0], ui.values[1]);
 			},
 			slide: function(event, ui) {
-				$('span.start_time').text(toHHMMSS(ui.values[0]));
-				$('span.stop_time').text(toHHMMSS(ui.values[1]));
+				sliderDiv.find('span.start_time').text(toHHMMSS(ui.values[0]));
+				sliderDiv.find('span.stop_time').text(toHHMMSS(ui.values[1]));
 			}
 	    });
 	};
