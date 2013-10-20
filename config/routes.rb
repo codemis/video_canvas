e104c6b9302cebb52bdcad6bd2c0b99c990bd440
Codemis <johnathan@missionaldigerati.com>
2013-10-20 03:23:12 -0700

    Removing the old annotations page.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Videocanvas::Application.routes.draw do
   # You can have the root of your site routed with "root"
 
   root 'pages#home'
-  get '/annotation' => 'pages#annotation', as: :annotation_page
   put '/create_guest_user' => 'users#create_guest_user', as: :create_guest_user
 
   # Example of regular route:
